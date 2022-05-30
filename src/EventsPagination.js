6bb46912ed8a124430e5007421101317a87da29b
Ivan Molto <ivanmolto@gmail.com>
2022-05-30 23:58:06 +0200

    feat: Style Events

--- src/EventsPagination.js
+++ src/EventsPagination.js
@@ -18,25 +18,45 @@ const EventsPagination = ({ count }) => {
   return (
     <>
       <Poaps eventId={eventId} />
-      {pageVariables.map((variables, i) => (
-        <AddressAttend key={i} variables={variables} />
-      ))}
-      {!isLastPage && (
-        <button
-          onClick={() =>
-            setPageVariables([
-              ...pageVariables,
-              {
-                offset: pageVariables.length * limit,
-                limit,
-                id: eventId,
-              },
-            ])
-          }
-        >
-          Load More
-        </button>
-      )}
+      <div className="mt-8">
+        <h2 className="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
+          Attendees
+        </h2>
+        {pageVariables.map((variables, i) => (
+          <AddressAttend key={i} variables={variables} />
+        ))}
+        {!isLastPage && (
+          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex flex-col mt-2">
+              <div className="align-middle min-w-full overflow-x-auto overflow-hidden sm:rounded-lg">
+                <div
+                  className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+                  aria-label="Pagination"
+                >
+                  <div className="flex-1 flex justify-between sm:justify-end">
+                    <button
+                      type="button"
+                      className="ml-3 relative inline-flex items-center px-4 py-2 border shadow-sm text-sm font-medium rounded-md text-white bg-sky-400 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-400"
+                      onClick={() =>
+                        setPageVariables([
+                          ...pageVariables,
+                          {
+                            offset: pageVariables.length * limit,
+                            limit,
+                            id: eventId,
+                          },
+                        ])
+                      }
+                    >
+                      Load more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- src/Poaps.js
+++ src/Poaps.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "urql";
+import { LightningBoltIcon, UserGroupIcon } from "@heroicons/react/outline";
 
 const Poaps = ({ eventId }) => {
   const variables = { id: eventId };
@@ -19,16 +20,64 @@ const Poaps = ({ eventId }) => {
   const [result] = useQuery({ query, variables });
   const { data, fetching, error } = result;
 
-  if (fetching || data === undefined) return <p>Loading...</p>;
+  if (fetching || data === undefined)
+    return (
+      <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+        <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+          <div className="flex-1 min-w-0">
+            <p>Loading...</p>
+          </div>
+        </div>
+      </div>
+    );
   if (error) return <p>{error.message}</p>;
   return (
     <>
       {data?.events.map((poap) => (
-        <div key={poap.id}>
-          <img src={poap.imageUrl} alt={poap.name} />
-          <div>{poap.id}</div>
-          <div>{poap.name}</div>
-          <div>{poap.addrsAttendAggregate.count}</div>
+        <div key={poap.id} className="bg-white shadow">
+          <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
+              <div className="flex-1 min-w-0">
+                <div className="flex items-center">
+                  <img
+                    className="hidden h-64 w-64 rounded-full sm:block"
+                    src={poap.imageUrl}
+                    alt={poap.name}
+                  />
+                  <div>
+                    <div className="flex items-center">
+                      <img
+                        className="h-16 w-16 rounded-full sm:hidden"
+                        src="https://s3.us-west-1.amazonaws.com/knn3-static-logo/43064.png"
+                        alt=""
+                      />
+                      <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                        {poap.name}
+                      </h1>
+                    </div>
+                    <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+                      <dt className="sr-only">Poap Id</dt>
+                      <dd className="flex items-center text-sm text-gray-500 font-medium capitalize sm:mr-6">
+                        <LightningBoltIcon
+                          className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        {poap.id}
+                      </dd>
+                      <dt className="sr-only">Supply</dt>
+                      <dd className="mt-3 flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
+                        <UserGroupIcon
+                          className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        {poap.addrsAttendAggregate.count}
+                      </dd>
+                    </dl>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </>

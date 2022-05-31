c6718760ef3821a9e90369c25ebb7f45715339ed
Ivan Molto <ivanmolto@gmail.com>
2022-05-31 14:58:21 +0200

    feat: Change color responsive icon

--- src/Layout.js
+++ src/Layout.js
@@ -232,7 +232,7 @@ const Layout = () => {
           <div className="relative z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200 lg:border-none">
             <button
               type="button"
-              className="px-4 border-r border-gray-200 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500 lg:hidden"
+              className="px-4 border-r border-gray-200 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500 lg:hidden"
               onClick={() => setSidebarOpen(true)}
             >
               <span className="sr-only">Open sidebar</span>
